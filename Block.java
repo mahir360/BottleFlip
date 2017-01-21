7322d0aac9ead4b496a1bf070f80ad36b0d066db
alexshi0000 <alexshi0000@gmail.com>
2017-01-21 15:20:48 -0500

    Create Block.java

---
+++ Block.java
@@ -0,0 +1,73 @@
+import java.awt.*;
+import java.util.*;
+
+public class Block{
+
+	static double speed = 1.2;
+	static ArrayList<Block> all = new ArrayList<Block>();
+	double x, y;
+	Color color;
+
+	Block(int x, int y, Color color){
+		this.color = color;
+		this.x = x;
+		this.y = y;
+		all.add(this);
+	}
+
+	public static void checkOut(){
+		for(int i = 0; i < Block.all.size(); i++){
+			if(Block.all.get(i).color == Color.black){
+				Block.all.remove(i);
+				i--;
+			}
+		}
+	}
+
+	public static void remove(int y){
+		int removed = 0;
+		for(int i = 0; i < Block.all.size(); i++){
+			Block a = Block.all.get(i);
+			for(int j = 0; j < Block.all.size(); j++){
+				Block b = Block.all.get(j);
+				if(a.y == y && b.y == y){
+					if(a != b && (a.x < b.x+50 && a.x > b.x) || (a.x+50 < b.x+50 && a.x+50 > b.x)){
+						Block.all.remove(j);
+						removed++;
+					}
+				}
+			}
+		}
+		for(int i = 0; i < removed; i++) generate(y);
+	}
+
+	public static void generate(int y){
+		int x = (int)(Math.random()*1150);
+		int red = (int)(Math.random()*2);
+		int blue = (int)(Math.random()*2);
+		int green = (int)(Math.random()*2);
+		if(red == 0 && green == 0 && blue == 0){
+			red = 1; green = 1; blue = 1;
+		}
+		new Block(x,y,new Color(red*255,green*255,blue*255));
+		remove(y);
+	}
+
+	public static void update(){
+		boolean removed = false;
+		for(int i = 0; i < all.size(); i++){
+			Block b = all.get(i);
+			b.y += speed;
+			if(b.y > 1210){
+				removed = true;
+				all.remove(i);
+				i--;
+			}
+		}
+		if(removed) {
+			for(int j = 0; j < Math.random()*2; j++){
+				generate(-10);
+			}
+		}
+	}
+}
